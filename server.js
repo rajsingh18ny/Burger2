be9f4de570e6df817af2c86761562625cd5e7ae6
Mona Lisa <rajsingh18ny@gmail.com>
2018-12-08 23:21:26 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controller/burger_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
+
+// NOTES: untrack and delete files from GITHUB
+// find . -name .DS_Store -print0 | xargs -0 git rm -f --ignore-unmatch
+// git rm -r --cached path_to_your_folder/
+
+
